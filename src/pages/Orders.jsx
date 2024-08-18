4c73a0519d49f336ffe132df96426b81ac23bc6a
featitx <siddabdul7@gmail.com>
2024-08-18 13:26:16 +0530

    almost-done

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject , Search , Toolbar} from '@syncfusion/ej2-react-grids';
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
+import { sort } from '@syncfusion/ej2-react-charts';
+
 
 const Orders = () => {
+
+  const toolbarOptions = ['Search'];
+
   return (
-    <div>
-      
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <Header category="page" title="order"/>
+
+    <GridComponent id='gridcomp' dataSource={ordersData} 
+    allowPaging 
+    allowSorting
+    toolbar={toolbarOptions}
+    >
+    <ColumnsDirective>
+    {
+      ordersGrid.map((item , index)=>(
+          <ColumnDirective
+           key={index}
+           {...item}
+          />
+      ))
+    }
+    </ColumnsDirective>
+    <Inject services={[ Resize , Sort , ContextMenu , Filter , Page , ExcelExport , Edit , PdfExport , Search , Toolbar ]}/>
+    </GridComponent>
+
     </div>
   )
 }
